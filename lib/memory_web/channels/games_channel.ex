35a506e41243c9dfa49d400d852a0df489fce7b8
Dan Daskivich <daskivich.d@husky.neu.edu>
2018-02-09 20:26:29 -0500

    Added an Agent process for game state backup.

--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -5,7 +5,7 @@ defmodule MemoryWeb.GamesChannel do
 
   def join("games:" <> name, payload, socket) do
     if authorized?(payload) do
-      game = Game.new()
+      game = Memory.GameBackup.load(name) || Game.new()
       socket = socket
       |> assign(:game, game)
       |> assign(:name, name)
@@ -15,26 +15,23 @@ defmodule MemoryWeb.GamesChannel do
     end
   end
 
-  def handle_in("name", payload, socket) do
-    xx = payload["xx"]
-    resp = %{"xx" => xx, "yy" => "Hello, " <> xx <> "!"}
-    {:reply, {:greeted, resp}, socket}
-  end
-
   def handle_in("select", %{"index" => ii}, socket) do
     game = Game.select(socket.assigns[:game], ii)
+    Memory.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
   def handle_in("match", %{}, socket) do
     game = Game.match(socket.assigns[:game])
+    Memory.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
   def handle_in("reset", %{}, socket) do
     game = Game.new()
+    Memory.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
